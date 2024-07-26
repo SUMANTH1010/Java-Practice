fc9e6ba444370663a30ec2230a27d6575c95a543
SUMANTH1010 <ysvsumanth1010@gmail.com>
2024-07-26 10:57:05 +0530

    Using String Buffer Class

--- StringReverse.java
+++ StringReverse.java
@@ -6,10 +6,17 @@ public class StringReverse{
         System.out.println("Enter any String : ");
         String input = sc.nextLine();
         System.out.println(input);
+
+        // Method 1 - Using StringBuilder Class
         StringBuilder input1 = new StringBuilder();
         input1.append(input);
         input1.reverse();
         System.out.println(input1);
+
+        // Method 2 - Using StringBuffer Class
+        StringBuffer input2 = new StringBuffer(input);
+        input2.reverse();
+        System.out.println(input2);
         sc.close();
     }
 }
